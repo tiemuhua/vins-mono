342e8d15e5343b4787788f34811ff3ad9de24874
tiemuhuaguo <tiemuhuaguo@tencent.com>
2023-06-09 23:08:32 +0800

    small change

--- vins/src/loop_closer/keyframe.h
+++ vins/src/loop_closer/keyframe.h
@@ -23,18 +23,18 @@ namespace vins{
         double relative_yaw = 0.0;
     };
 
-    class KeyFrame {
-    public:
+    struct KeyFrame {
         KeyFrame(double _time_stamp, Eigen::Vector3d &t, Eigen::Matrix3d &r, cv::Mat &_image,
                  std::vector<cv::Point3f> &_point_3d, std::vector<cv::Point2f> &_point_2d_uv);
 
-        void getVioPose(Eigen::Vector3d &_T_w_i, Eigen::Matrix3d &_R_w_i) const;
         void getPose(Eigen::Vector3d &_T_w_i, Eigen::Matrix3d &_R_w_i) const;
+        void getVioPose(Eigen::Vector3d &_T_w_i, Eigen::Matrix3d &_R_w_i) const;
         void getPosRotDrift(const Eigen::Vector3d &pos, const Eigen::Vector3d &euler,
                             Eigen::Vector3d &pos_drift, Eigen::Matrix3d &rot_drift) const;
+
         void updatePose(const Eigen::Vector3d &_T_w_i, const Eigen::Matrix3d &_R_w_i);
-        void updatePoseByDrift(const Eigen::Vector3d &t_drift, const Eigen::Matrix3d &r_drift);
         void updateVioPose(const Eigen::Vector3d &_T_w_i, const Eigen::Matrix3d &_R_w_i);
+        void updatePoseByDrift(const Eigen::Vector3d &t_drift, const Eigen::Matrix3d &r_drift);
 
         double time_stamp;
         Eigen::Vector3d vio_T_i_w_;
@@ -50,11 +50,6 @@ namespace vins{
         vector<DVision::BRIEF::bitset> external_descriptors_;
 
         LoopInfo loop_info_;
-    private:
-        void PnPRANSAC(const vector<cv::Point2f> &matched_2d_old_norm,
-                       const std::vector<cv::Point3f> &matched_3d,
-                       std::vector<uchar> &status,
-                       Eigen::Vector3d &PnP_T_old, Eigen::Matrix3d &PnP_R_old);
     };
     typedef std::shared_ptr<KeyFrame> KeyFramePtr;
     typedef const std::shared_ptr<const KeyFrame> ConstKeyFramePtr;
