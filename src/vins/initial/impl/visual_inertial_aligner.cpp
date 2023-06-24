ed099da53326a3eba2dddf27a4b36fe39c500aeb
tiemuhuaguo <tiemuhuaguo@tencent.com>
2023-06-24 18:19:54 +0800

    移出滑动窗口初始化

--- src/vins/initial/impl/visual_inertial_aligner.cpp
+++ src/vins/initial/impl/visual_inertial_aligner.cpp
@@ -190,9 +190,6 @@ namespace vins {
 
         // todo solveGyroscopeBias求出来的是bg的值还是bg的变化值？
         Eigen::Vector3d delta_bg = solveGyroscopeBias(all_frames);
-        for (int i = 0; i < bg_window.size(); ++i) {
-            bg_window.at(i) = bg_window.at(i) + delta_bg;
-        }
 
         for (int i = 0; i < frame_size - 1; ++i) {
             all_frames[i].pre_integrate_.rePredict(Eigen::Vector3d::Zero(), delta_bg);
@@ -213,32 +210,6 @@ namespace vins {
         })->R;
         Eigen::Matrix3d rot_diff = rotGravityToZAxis(gravity, R0);
         gravity = rot_diff * gravity;
-        for (int frame_id = 0, key_frame_id = 0; frame_id < frame_size; frame_id++) {
-            if (!all_frames[frame_id].is_key_frame_) {
-                continue;
-            }
-            rot_window.at(key_frame_id) = rot_diff * all_frames.at(key_frame_id).R;
-            pos_window.at(key_frame_id) = rot_diff * all_frames.at(key_frame_id).T;
-            vel_window.at(key_frame_id) = rot_diff * velocities.at(key_frame_id);
-            key_frame_id++;
-        }
-        for (int i = 0; i < (int) pos_window.size(); ++i) {
-            pos_window.at(i) = s * pos_window.at(i) - rot_window.at(i) * TIC - (s * pos_window.at(i) - rot_window.at(i) * TIC);
-        }
-
-        for (int i = 0; i < (int) pre_integrate_window.size(); ++i) {
-            pre_integrate_window.at(i).rePredict(Eigen::Vector3d::Zero(), bg_window.at(i));
-        }
-
-        //triangulate on cam pose , no tic
-        feature_manager.clearDepth();
-        feature_manager.triangulate(pos_window, rot_window, Eigen::Vector3d::Zero(), RIC);
-        for (FeaturesOfId &features_of_id: feature_manager.features_) {
-            if (features_of_id.feature_points_.size() < 2 || features_of_id.start_frame_ >= Param::Instance().window_size - 2) {
-                continue;
-            }
-            features_of_id.inv_depth *= s; // todo tiemuhuaguo 这里是乘还是除？？
-        }
 
         return true;
     }
---
+++ src/vins/initial/initiate.h
@@ -0,0 +1,14 @@
+//
+// Created by gjt on 6/24/23.
+//
+
+#ifndef GJT_VINS_INITIATE_H
+#define GJT_VINS_INITIATE_H
+
+
+class initiate {
+
+};
+
+
+#endif //GJT_VINS_INITIATE_H
