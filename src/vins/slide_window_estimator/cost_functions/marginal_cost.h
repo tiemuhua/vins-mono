a9058c28c861ec08a5447312952b04771ef0cb1c
tiemuhuaguo <tiemuhuaguo@tencent.com>
2023-07-22 09:56:45 +0800

    注释

--- src/vins/slide_window_estimator/cost_functions/marginal_cost.h
+++ src/vins/slide_window_estimator/cost_functions/marginal_cost.h
@@ -54,22 +54,24 @@ namespace vins {
          * 2.保留量:与丢弃量有约束边相连，但是尚不从滑动窗口中丢弃的量
          *   存在于原始量、冻结量和更新量当中
          * 3.无关量:既不需要从滑动窗口中丢弃，也不和丢弃量相连，这些量不卷入边缘化过程
-         *   在原始量、冻结量更新量当中均不存在
+         *   在原始量、冻结量、更新量当中均不存在
          *
          * 将待优化变量在时间上分为：
          * 1.原始量x_original:
-         *   addMetaFactor()和marginalize()时MarginalMetaFactor.parameter_blocks_对应的数据，此时优化过程尚未开始.
-         *   原始量本质上是slide_window_estimator.cpp中的c_pos、c_vel等数组
-         *   marginalize()会计算残差R和保留原始量的雅可比J，保存在reserve_param_residuals_和reserve_param_jacobians_中
-         * 2.冻结量x_frozen:
-         *   marginalize()会将保留原始量复制一份副本，储存在reserve_block_frozen_中。
-         *   @param reserve_block_origin 持有保留原始量，且顺序上和reserve_block_frozen_一一对应，其内容同样是c_pos、c_vel等数组。
-         * 3.更新量x_updated:
-         *   保留原始量在窗口中滑动后被塞入MarginalCost，并在后续优化中不断更新，更新后的量称之为更新量。
+         *   上一轮优化过程结束后，边缘化过程中，即addMetaFactor()和marginalize()时对应的数据.
+         *   保存在MarginalMetaFactor.parameter_blocks_中，指向slide_window_estimator.cpp中c_pos、c_vel等数组
+         * 2.更新量x_updated:
+         *   保留原始量在窗口中滑动后被塞入MarginalCost，并在下一轮优化中不断更新，更新后的量称之为更新量。
          *   更新量本质上同样是slide_window_estimator.cpp中的c_pos、c_vel等数组，
-         *   但由于滑动窗口的滑动，同一变量在更新量和原始量中的地址并不相同，s_slide_addr_map描述了从原始量到更新量的地址映射。
+         *   但由于滑动窗口的滑动，同一变量在更新量和原始量中的地址并不相同，s_slide_addr_map描述了从保留原始量到更新量的地址映射。
+         * 3.冻结量x_frozen:
+         *   由于c_pos、c_vel中的数据在后续优化中被不断更新，
+         *   marginalize()在reserve_block_frozen_中储存了一份保留原始量的快照，称之为冻结量。
+         *   冻结量在下一轮优化中保持不变。
+         *   @param reserve_block_origin 持有保留原始量，且顺序上和reserve_block_frozen_一一对应，其内容同样是c_pos、c_vel等数组。
          *
          * 优化过程：
+         *   marginalize()计算残差R和保留原始量的雅可比J，保存在reserve_param_residuals_和reserve_param_jacobians_中
          *   后续优化会通过x_updated与x_frozen的差来估计残差，即更新后的残差为 r = R + J * (x_updated - x_frozen)
          *   由于reserve_block_origin和sp_slide_addr_map构建起了冻结量和更新量之间的地址对应关系，这样的减法是可行的。
          * */
