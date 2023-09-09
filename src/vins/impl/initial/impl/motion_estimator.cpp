81a95fe918f7185176fc7abfc2bd669846193a0d
tiemuhuaguo <tiemuhuaguo@tencent.com>
2023-09-09 15:46:33 +0800

    使用cv::cv2eigen

--- src/vins/impl/initial/impl/motion_estimator.cpp
+++ src/vins/impl/initial/impl/motion_estimator.cpp
@@ -1,13 +1,12 @@
 #include "motion_estimator.h"
 #include <opencv2/calib3d.hpp>
+#include <Eigen/Eigen>
+#include <opencv2/core/eigen.hpp>
 
 // todo tiemuhuaguo 坐标系搞混了，应该是在l坐标系中r相对于l的旋转和位移
 namespace vins{
     bool MotionEstimator::solveRelativeRT(const Correspondences &correspondences,
-                                          Eigen::Matrix3d &Rotation, Eigen::Vector3d &Translation) {
-        if (correspondences.size() < 15) {
-            return false;
-        }
+                                          Eigen::Matrix3d &rotation, Eigen::Vector3d &translation) {
         std::vector<cv::Point2f> ll, rr;
         for (const auto & correspondence : correspondences) {
             ll.emplace_back(correspondence.first);
@@ -16,19 +15,14 @@ namespace vins{
         cv::Mat mask;
         cv::Mat E = cv::findFundamentalMat(ll, rr, cv::FM_RANSAC, 0.3 / 460, 0.99, mask);
         cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 1, 0, 0, 0, 1, 0, 0, 0, 1);
-        cv::Mat rot, trans;
-        int inliner_cnt = cv::recoverPose(E, ll, rr, cameraMatrix, rot, trans, mask);
-
-        for (int i = 0; i < 3; i++) {
-            Translation(i) = trans.at<double>(i, 0);
-            for (int j = 0; j < 3; j++)
-                Rotation(i, j) = rot.at<double>(i, j);
-        }
-        if (inliner_cnt > 12)
-            return true;
-        else
+        cv::Mat cv_rot, cv_trans;
+        int inliner_cnt = cv::recoverPose(E, ll, rr, cameraMatrix, cv_rot, cv_trans, mask);
+        if (inliner_cnt < 13) {
             return false;
-
+        }
+        cv::cv2eigen(cv_rot, rotation);
+        cv::cv2eigen(cv_trans, translation);
+        return true;
     }
 }
 
