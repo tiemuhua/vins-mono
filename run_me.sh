9bf068eeca47264ccd39c4879f727dc7c112d57f
tiemuhuaguo <tiemuhuaguo@tencent.com>
2023-09-22 10:14:58 +0800

    数据集脚本

---
+++ run_me.sh
@@ -0,0 +1,7 @@
+mkdir dataset && cd dataset
+curl https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/2011_09_26_drive_0001/2011_09_26_drive_0001_extract.zip --output 2011_09_26_drive_0001_extract.zip
+curl https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/2011_09_26_calib.zip --output 2011_09_26_calib.zip
+curl https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/2011_09_26_drive_0001/2011_09_26_drive_0001_tracklets.zip --output 2011_09_26_drive_0001_tracklets.zip
+unzip -d tracklets 2011_09_26_drive_0001_tracklets.zip
+unzip -d extract 2011_09_26_drive_0001_extract.zip
+unzip -d calib 2011_09_26_calib.zip
