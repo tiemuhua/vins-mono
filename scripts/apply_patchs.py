59401d0739ee21c363e5eca136b50eff8d31c30a
tiemuhuaguo <tiemuhuaguo@tencent.com>
2023-10-13 08:51:34 +0800

    patch+编译三方库的脚本

---
+++ scripts/apply_patchs.py
@@ -0,0 +1,27 @@
+
+import platform
+import os
+import subprocess
+
+PLATFORM:str = platform.platform()
+THIRD_PATH:str = os.path.split(os.path.realpath(__file__))[0] + '/../3rd/'
+INSTALL_FOLDER:str = "install_" + PLATFORM
+
+def call_shell(cmd:str)->int:
+    return subprocess.run(cmd.split(" "))
+
+def apply_patchs():
+    os.chdir(THIRD_PATH)
+    directories:list[str] = os.listdir(".")
+    for dir in directories:
+        if os.path.isfile(dir):
+            continue
+        os.chdir(dir)
+        if not os.path.exists("../patch/" + dir):
+            print("warning: there is no directory " + dir + "in patch folder")
+        else:
+            call_shell("git apply ../patch/" + dir + "/0001-build.patch")
+        os.chdir("..")
+
+if __name__ == '__main__':
+    apply_patchs()
---
+++ scripts/compilt_third_libs.py
@@ -0,0 +1,55 @@
+
+import platform
+import os
+import subprocess
+
+PLATFORM:str = platform.platform()
+THIRD_PATH:str = os.path.split(os.path.realpath(__file__))[0] + '../3rd/'
+INSTALL_FOLDER:str = "install_" + PLATFORM
+
+def call_shell_with_env(cmd:str, env:dict[str, str])->int:
+    return subprocess.run(cmd.split(" "), env=env)
+
+def compile_third_libs():
+    my_env:dict[str, str] = os.environ.copy()
+    os.chdir(THIRD_PATH)
+    os.chdir("Boost")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["Boost_DIR"] = THIRD_PATH + "Boost/" + INSTALL_FOLDER + "/lib/cmake/" + "/Boost-1.82.0/"
+
+    os.chdir("../Eigen3")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["Eigen3_DIR"] = THIRD_PATH + "Eigen3/" + INSTALL_FOLDER + "/share/eigen3/cmake/"
+
+    os.chdir("../opencv")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["OpenCV_DIR"] = THIRD_PATH + "opencv/" + INSTALL_FOLDER + "/lib/cmake/" + "/opencv4"
+
+    os.chdir("../gflags")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["gflags_DIR"] = THIRD_PATH + "gflags/" + INSTALL_FOLDER + "/lib/cmake/" + "/gflags"
+
+    os.chdir("../glog")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["glog_DIR"] = THIRD_PATH + "glog/" + INSTALL_FOLDER + "/lib/cmake/" + "/glog"
+
+    os.chdir("../Ceres")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["Ceres_DIR"] = THIRD_PATH + "Ceres/" + INSTALL_FOLDER + "/lib/cmake/" + "/Ceres"
+
+    os.chdir("../DLib")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["DLib_DIR"] = THIRD_PATH + "DLib/" + INSTALL_FOLDER + "/lib/cmake/" + "/DLib"
+
+    os.chdir("../DBoW2")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["DBoW2_DIR"] = THIRD_PATH + "DBoW2/" + INSTALL_FOLDER + "/lib/cmake/" + "/DBoW2"
+
+    os.chdir("../camodocal")
+    call_shell_with_env("python3.10 build.py", my_env)
+    my_env["camodocal_DIR"] = THIRD_PATH + "camodocal/" + INSTALL_FOLDER + "/lib/cmake/" + "/camodocal"
+    os.chdir("..")
+
+
+if __name__ == '__main__':
+    compile_third_libs()
